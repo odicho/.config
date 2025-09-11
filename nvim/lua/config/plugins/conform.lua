7070d972a4bd1a77326d6a7f95b06f643c1e035b
Amanuel <amanuel.odicho@smartsheet.com>
2025-09-11 12:55:41 -0500

    update

--- nvim/lua/config/plugins/conform.lua
+++ nvim/lua/config/plugins/conform.lua
@@ -16,9 +16,17 @@ return {
     local conform = require("conform")
     conform.setup(opts)
 
+    vim.keymap.set("n", "<bs>f", function()
+      conform.format({ async = true, lsp_fallback = true })
+    end, { desc = "Format file with Prettier" })
+
     vim.keymap.set("n", "<leader>f", function()
       conform.format({ async = true, lsp_fallback = true })
     end, { desc = "Format file with Prettier" })
+
+    vim.keymap.set("v", "<bs>f", function()
+      conform.format({ async = true, lsp_fallback = true })
+    end, { desc = "Format selection with Prettier" })
 
     vim.keymap.set("v", "<leader>f", function()
       conform.format({ async = true, lsp_fallback = true })
