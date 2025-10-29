e24e7e999008eb8b9b941d14b1684e8aebc38cbb
Amanuel <amanuel.odicho@smartsheet.com>
2025-10-28 20:26:15 -0500

    adding go for C-]??

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -4,11 +4,11 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<cr>")
 vim.keymap.set("n", "<leader>x", ":.lua<cr>", { silent = true, desc = "Execute current line as Lua" })
 vim.keymap.set("v", "<leader>x", ":lua<cr>", { silent = true, desc = "Execute selected Lua" })
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set('n', '<esc>', '<cmd>nohlsearch<cr>')
-vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'move focus to the left window' })
-vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'move focus to the right window' })
-vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'move focus to the lower window' })
-vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'move focus to the upper window' })
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<cr>")
+vim.keymap.set("n", "<c-h>", "<c-w><c-h>", { desc = "move focus to the left window" })
+vim.keymap.set("n", "<c-l>", "<c-w><c-l>", { desc = "move focus to the right window" })
+vim.keymap.set("n", "<c-j>", "<c-w><c-j>", { desc = "move focus to the lower window" })
+vim.keymap.set("n", "<c-k>", "<c-w><c-k>", { desc = "move focus to the upper window" })
 
 -- Center while moving vertically
 vim.keymap.set("n", "n", "nzzzv", { noremap = true })
@@ -28,6 +28,7 @@ vim.keymap.set("n", "<bs><Tab>", ":b#<CR>", { silent = true, desc = "Switch to a
 vim.keymap.set("n", "<leader><Tab>", ":b#<CR>", { silent = true, desc = "Switch to alternate buffer" })
 vim.keymap.set("n", "<bs>d", ":bd<CR>", { silent = true, desc = "Delete current buffer" })
 vim.keymap.set("n", "<leader>d", ":bd<CR>", { silent = true, desc = "Delete current buffer" })
+vim.keymap.set("n", "go", "<C-]>zz", { noremap = true, desc = "Go to definition (Cmd+])" })
 
 local alt_arrow_keys = {
   up = { "<A-Up>", "<A-k>" },
